452f5ccc5d4b826b3357c873e4dbde73e11ccddb
gaoyiling <gaoyiling@dataocean.com.cn>
2019-06-28 14:36:07 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './me.jpg';
+import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello','webpack'],' ');
+    element.innerHTML = _.join(['Hello','Spring'],' ');
+    element.classList.add('hello');
+
+    //添加图像
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }
