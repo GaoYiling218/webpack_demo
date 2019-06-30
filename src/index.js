364cb904d090706b2e36ab232c5bb2db7d76148e
gaoyiling <gaoyiling@dataocean.com.cn>
2019-06-30 16:29:51 +0800

    webpack资源管理

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './me.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
+import './styles.css';
 
 function component() {
-    let element = document.createElement('div');
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello','Spring'],' ');
-    element.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    //添加图像
-    var myIcon = new Image();
-    myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;  // onclick 事件绑定原始的 printMe 函数上
 
-    element.appendChild(myIcon);
+  element.appendChild(btn);
 
-    console.log(Data);
-
-    return element;
+  return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    mode:'development',
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
+                use: ['style-loader', 'css-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        // new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '模块热更替'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
