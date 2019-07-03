555674c8ed685bbb498aa4dbf6fb27fbbd1a08fe
gaoyiling <gaoyiling@dataocean.com.cn>
2019-07-03 17:06:26 +0800

    生产环境

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+import { cube } from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Look like we are in development mode!');
+}
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;  // onclick 事件绑定原始的 printMe 函数上
-
-  element.appendChild(btn);
+  var element = document.createElement('pre');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
-let element = component(); // 存储 element，以在 print.js 修改时重新渲染
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  })
-}
+document.body.appendChild(component());
