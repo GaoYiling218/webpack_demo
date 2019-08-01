e42fe862f9bdd00b3221c6b71a3982ee06981955
gaoyiling <gaoyiling@dataocean.com.cn>
2019-08-01 15:27:59 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import { cube } from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Look like we are in development mode!');
-}
-
 function component() {
-  var element = document.createElement('pre');
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to' + cube(5)
-  ].join('\n\n');
+  let element = document.createElement('div');
+
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 const path = require('path');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
+    entry:  './src/index.js',
     plugins: [
-        // new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Production'
+            title: 'Caching'
         })
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+   
 };
