4b2c6fd9c23b93097487ef2cc117856d8a02bad6
gaoyiling <gaoyiling@dataocean.com.cn>
2019-08-01 16:02:02 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-function component() {
-  let element = document.createElement('div');
+import _ from 'lodash';
+// import Print from './print';
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+function component() {
+  var element = document.createElement('div');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.onclick = Print.bind(null, 'Hello webpack!');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const webpack = require('webpack');
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:  './src/index.js',
+    entry: './src/index.js',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Caching'
-        })
+        }),
+        new webpack.HashedModuleIdsPlugin()
     ],
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
-   
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+
 };
